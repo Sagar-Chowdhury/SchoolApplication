bc9e82e25ff8a41f0b3cee59e796745b6e3a6691
Sagar Chowdhury <sagarchowdhury4693@gmail.com>
2024-01-19 18:44:17 +0530

    Server Side bean validation done

--- src/main/java/com/example/SchoolApp/controller/ContactController.java
+++ src/main/java/com/example/SchoolApp/controller/ContactController.java
@@ -2,14 +2,20 @@ package com.example.SchoolApp.controller;
 
 import com.example.SchoolApp.model.Contact;
 import com.example.SchoolApp.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
+@Slf4j
 public class ContactController {
 
     private final ContactService contactService;
@@ -20,16 +26,20 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage(){
+    public String displayContactPage(Model model){
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @RequestMapping(value = "/saveMsg" , method = POST)
-    public ModelAndView saveMessage(Contact contact)
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors)
     {
-       contactService.saveMessageDetails(contact);
-       System.out.println(contact.toString());
-      return new ModelAndView("redirect:/contact");
+        if(errors.hasErrors()){
+            log.error("Contact Form Validation failed due to :"+errors.toString());
+            return "contact.html";
+        }
+        contactService.saveMessageDetails(contact);
+        return "redirect:/contact";
     }
 
 
--- src/main/java/com/example/SchoolApp/model/Contact.java
+++ src/main/java/com/example/SchoolApp/model/Contact.java
@@ -1,15 +1,33 @@
 package com.example.SchoolApp.model;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Contact {
 
+    @NotBlank(message = "Name Must Not be Blank")
+    @Size(min = 3, message = "Name Must be at least 3 characters long")
     private String name;
+
+    @NotBlank(message = "Mobile No must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})" , message ="Mobile Number must be 10 digits")
     private String mobileNumber;
+
+    @NotBlank(message = "Email Should not be blank")
+    @Email(message = "Please Provide Email Address")
     private String email;
+
+    @NotBlank(message = "Subject Must not be blank")
+    @Size(min=5, message = "Subject Must be at least 5 characters long")
     private String subject;
+
+    @NotBlank(message = "Message must not be blank")
+    @Size(min=10, message="Message must be at least 10 characters long")
     private String message;
 
 
