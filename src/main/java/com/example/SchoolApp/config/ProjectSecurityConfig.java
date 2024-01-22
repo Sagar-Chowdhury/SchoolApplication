dd8a0a32cd009442bc09073192b3cc7353498325
Sagar Chowdhury <sagarchowdhury4693@gmail.com>
2024-01-23 01:13:04 +0530

    Began work on adding login/logout functionality

--- src/main/java/com/example/SchoolApp/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/SchoolApp/config/ProjectSecurityConfig.java
@@ -3,10 +3,12 @@ package com.example.SchoolApp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,20 +17,32 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/home").authenticated()
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/dashboard").authenticated()
+                        .requestMatchers("/home").permitAll()
                         .requestMatchers("/holidays/**").permitAll()
                         .requestMatchers("/contact").permitAll()
                         .requestMatchers("/saveMsg").permitAll()
                         .requestMatchers("/courses").permitAll()
                         .requestMatchers("/assets/**").permitAll()
-                        .anyRequest().authenticated()
-                        )
-                        .httpBasic(withDefaults());
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/dashboard")
+                        .failureUrl("/login?error=true").permitAll()
+
+                )
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/login?logout=true").permitAll()
+                        .invalidateHttpSession(true).permitAll())
+                .httpBasic(withDefaults());
         return http.build();
     }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsService(){
+    public InMemoryUserDetailsManager userDetailsService() {
 
         UserDetails user = User.withDefaultPasswordEncoder()  //deprecated since insecure for production user.
                 .username("user")
@@ -37,11 +51,11 @@ public class ProjectSecurityConfig {
                 .build();
 
         UserDetails admin = User.withDefaultPasswordEncoder()
-                            .username("admin")
-                             .password("54321")
-                             .roles("USER","ADMIN")
-                              .build();
-        return new InMemoryUserDetailsManager(user,admin);
+                .username("admin")
+                .password("54321")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
 
     }
 
