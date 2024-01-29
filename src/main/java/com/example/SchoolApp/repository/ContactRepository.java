907bb90d9a68801c593fa52c963098f1c1865a00
Sagar Chowdhury <sagarchowdhury4693@gmail.com>
2024-01-29 20:44:54 +0530

    Migration to MySql db

--- src/main/java/com/example/SchoolApp/repository/ContactRepository.java
+++ src/main/java/com/example/SchoolApp/repository/ContactRepository.java
@@ -4,8 +4,11 @@ import com.example.SchoolApp.model.Contact;
 import com.example.SchoolApp.rowmappers.ContactRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,26 +20,32 @@ public class ContactRepository {
     @Autowired
     public ContactRepository(JdbcTemplate jdbcTemplate){ this.jdbcTemplate=jdbcTemplate; }
 
-    public int saveContactMsg(Contact contact) {
-        String sql = "INSERT INTO CONTACT_MSG (NAME,MOBILE_NUM,EMAIL,SUBJECT,MESSAGE,STATUS,CREATED_AT,CREATED_BY) " +
-                "VALUES (?,?,?,?,?,?,?,?)";
-        return jdbcTemplate.update(sql, contact.getName(), contact.getMobileNumber(),
-                contact.getEmail(), contact.getSubject(), contact.getMessage(),
-                contact.getStatus(), contact.getCreatedAt(), contact.getCreatedBy());
+    public int saveContactMsg(Contact contact){
+        String sql = "insert into contact_msg (name,mobile_num,email,subject,message,status," +
+                "created_at,created_by) values (?,?,?,?,?,?,?,?)";
+        return jdbcTemplate.update(sql,contact.getName(),contact.getMobileNumber(),
+                contact.getEmail(),contact.getSubject(),contact.getMessage(),
+                contact.getStatus(),contact.getCreatedAt(),contact.getCreatedBy());
     }
 
-    public List<Contact> findMsgsWithStatus(String status){
-        String sql = "SELECT * FROM CONTACT_MSG WHERE STATUS = ?";
-        return jdbcTemplate.query(sql,preparedStatement -> preparedStatement.setString(1, status),new ContactRowMapper());
+    public List<Contact> findMsgsWithStatus(String status) {
+        String sql = "select * from contact_msg where status = ?";
+        return jdbcTemplate.query(sql,new PreparedStatementSetter() {
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, status);
+            }
+        },new ContactRowMapper());
     }
 
     public int updateMsgStatus(int contactId, String status,String updatedBy) {
-        String sql = "UPDATE CONTACT_MSG SET STATUS = ?, UPDATED_BY = ?,UPDATED_AT =? WHERE CONTACT_ID = ?";
-        return jdbcTemplate.update(sql, preparedStatement -> {
-            preparedStatement.setString(1, status);
-            preparedStatement.setString(2, updatedBy);
-            preparedStatement.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
-            preparedStatement.setInt(4, contactId);
+        String sql = "update contact_msg set status = ?, updated_by = ?,updated_at =? where contact_id = ?";
+        return jdbcTemplate.update(sql,new PreparedStatementSetter() {
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, status);
+                preparedStatement.setString(2, updatedBy);
+                preparedStatement.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+                preparedStatement.setInt(4, contactId);
+            }
         });
     }
 
